3cd904209f8be34e85b62c92613ae5bee5234a37
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-29 23:12:41 -0300

    Changes

--- src/consumers/consumer-lambda-to-rabbimq.js
+++ src/consumers/consumer-lambda-to-rabbimq.js
@@ -2,11 +2,18 @@ require("dotenv").config({ path: ".env" })
 
 const AWS = require("aws-sdk")
 const amqp = require('amqplib');
+const https = require('https');
+const agent = new https.Agent({
+  keepAlive: true
+});
 
 const credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
 AWS.config.credentials = credentials;
 
 const lambdaClient = new AWS.Lambda({
+    httpOptions: {
+        agent
+    },
     region: "us-east-1"
 })
 
@@ -45,64 +52,3 @@ const startConsumer = async () => {
 };
 
 startConsumer()
-// const ses = new AWS.SES(
-//     {
-//         region: "us-east-1"
-//     }
-// )
-
-// const sendEmail = async (data) => {
-//     console.log(`>>> ${new Date().toISOString()} | Sending email`)
-//     await ses.sendEmail({
-//         Source: "tiagorosadacost@gmail.com",
-//         Destination: {
-//             ToAddresses: [data.to],
-//         },
-//         Message: {
-//             Subject: {
-//                 Data: "Test"
-//             },
-//             Body: {
-//                 Text: {
-//                     Data: data.message
-//                 }
-//             }
-//         }
-//     }).promise()
-//     console.log(`>>> ${new Date().toISOString()} | Sended email`)
-// }
-
-// const sleep = (seconds) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => resolve(), (seconds * 1000))
-//     })
-// }
-
-// const app = Consumer.create({
-//     queueUrl: process.env.QUEUE,
-//     handleMessage: async (message) => {
-//         console.time()
-//         console.log(">>>>>>>> Processing...")
-//         const data = JSON.parse(message.Body)
-//         console.log(data);
-//         const promisesToSendEmail = []
-//         for (let index = 0; index < data.length; index++) {
-//             promisesToSendEmail.push(
-//                 sendEmail(data[index])
-//             )
-//         }
-//         await Promise.all(promisesToSendEmail)
-//         console.log(">>>>>>>> Finish")
-//         console.timeEnd()
-//     }
-// });
-
-// app.on('error', (err) => {
-//     console.error(err.message);
-// });
-
-// app.on('processing_error', (err) => {
-//     console.error(err.message);
-// });
-
-// app.start();
